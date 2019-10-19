ecea3c5677fb9a7125a6b2af98b9db3de5d0b269
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-19 04:04:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  num = 0 
+  is_negative = number < 0 
+  if is_negative == true 
+    num = number / - 1 
+  end
+  array = (1..number).to_a
+  array.each_with_index do |i|
+    remainder = number % i 
+    if remainder == 0 && i != number && i != 1
+      return false
+  end
+ end
+ return true 
+end  
+
+
